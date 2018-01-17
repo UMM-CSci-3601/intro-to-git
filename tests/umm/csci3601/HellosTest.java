9ed0376f4aaac71b9da24cbd81cb159a2f01e0f0
Nic McPhee <mcphee@morris.umn.edu>
2018-01-17 15:58:41 -0600

    Support more complex names
    The initial test only support single-word, ASCI names with no
    punctuation (e.g., hyphens). This uses [a regex from StackOverflow](https://stackoverflow.com/questions/15805555/java-regex-to-validate-full-name-allow-only-spaces-and-letters)
    to support a broader range of names.
    
    It also turns out that I totally forgot to include an assertion when
    checking that the first line was correct. This also fixes that.

--- tests/umm/csci3601/HellosTest.java
+++ tests/umm/csci3601/HellosTest.java
@@ -2,9 +2,6 @@ package umm.csci3601;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -13,7 +10,7 @@ public class HellosTest {
     public void testLineStructure() {
         String output = Hellos.generateOutput();
         String[] lines = output.split("\n");
-        Hellos.WELCOME_LINE.equals(lines[0]);
+        assertEquals(Hellos.WELCOME_LINE, lines[0], "Welcome line isn't correct.");
 
         // This regex supports unicode letters spaces, apostrophes, and hyphens
         // Taken from https://stackoverflow.com/questions/15805555/java-regex-to-validate-full-name-allow-only-spaces-and-letters
