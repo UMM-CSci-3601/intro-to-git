da84c2b1a13bcb53c3c7ec41d2d9c208cc2dcd61
K.K. Lamberty <lamberty@morris.umn.edu>
2018-12-09 14:02:11 -0600

    Update HellosTest.java to fix issue #20, bring back test from last year
    testLineStructure() was added last year and was still visible in the master branch, but was not in the develop branch. This commit brings that test here and makes it work with the version of testing we are using. It further updates the test so that the greeting does not need to be 'Hello!', but does still need to have the '!'

--- src/test/java/HellosTest.java
+++ src/test/java/HellosTest.java
@@ -6,6 +6,22 @@ import static org.hamcrest.CoreMatchers.*;
 
 
 public class HellosTest {
-	
+
+    @Test
+    public void testLineStructure() {
+        String output = Hellos.generateOutput();
+        String[] lines = output.split("\n");
+        assertEquals("Welcome line isn't correct.", Hellos.WELCOME_LINE, lines[0]);
+
+        // This regex supports unicode letters spaces, apostrophes, and hyphens
+        // Taken from https://stackoverflow.com/questions/15805555/java-regex-to-validate-full-name-allow-only-spaces-and-letters
+        // The pattern needs to include some name, the "says", and the single quotes... and they must speak with emphasis
+        // as noted by the required "!"
+        String linePattern = "[\\p{L} .'-]+ says '+[\\p{L} .'-]+!'";
+
+        for (int i=1; i<lines.length; ++i) {
+            assertTrue("Line <" + lines[i] + "> doesn't match", lines[i].matches(linePattern));
+        }
+    }
 
 }
