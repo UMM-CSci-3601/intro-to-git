cf47410dafd60a74fe2a9aed77c07923aba159fa
Nic McPhee <mcphee@morris.umn.edu>
2021-01-17 18:34:53 -0600

    Modify the checkstyle config and eliminate warnings
    I turned off all the JavaDoc warnings because we don't use JavaDoc these days. I also turned off DesignForExtension because that doesn't make sense for an app (according to the docs). Lastly, I extended the acceptable line length from 80 to 120 characters.
    
    I then fixed the remaining warnings by making the one parameter final, and shortening some really long lines.

--- src/test/java/hellos/HellosTest.java
+++ src/test/java/hellos/HellosTest.java
@@ -1,10 +1,12 @@
 package hellos;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class HellosTest {
 
@@ -21,13 +23,19 @@ public class HellosTest {
   @Test
   public void testLineStructure() {
     // This regex supports unicode letters spaces, apostrophes, and hyphens
-    // Taken from https://stackoverflow.com/questions/15805555/java-regex-to-validate-full-name-allow-only-spaces-and-letters
-    // The pattern needs to include some name, the "says", and the single quotes... and they must speak with emphasis
-    // as noted by the required "!"
+    // Taken from
+    // https://stackoverflow.com/questions/15805555/java-regex-to-validate-full-name-allow-only-spaces-and-letters
+    // The pattern needs to include
+    //   * some name,
+    //   * the "says",
+    //   * and the single quotes, and
+    //   * they must speak with emphasis as noted by the required "!"
     String linePattern = "[\\p{L} .'-]+ says '+[\\p{L} .'-]+!'";
 
     for (int i = 0; i < lines.length; ++i) {
-      assertTrue(lines[i].matches(linePattern), "Line <" + lines[i] + "> doesn't match pattern '<Name> says <greeting>!'");
+      assertTrue(
+        lines[i].matches(linePattern),
+        "Line <" + lines[i] + "> doesn't match pattern '<Name> says <greeting>!'");
     }
   }
 
@@ -41,7 +49,9 @@ public class HellosTest {
     // confirm that the generated output is in alphabetical order.
     for (int i = 0; i < lines.length; ++i) {
       assertEquals(sortedLines[i], lines[i],
-        "Line " + i + " doesn't match: lines[" + i + "] is: " + lines[i] + " and sortedLines[" + i + "] is: " + sortedLines[i]);
+        "Sorted lines don't match: "
+          + "lines[" + i + "] is: " + lines[i]
+          + " and sortedLines[" + i + "] is: " + sortedLines[i]);
     }
   }
 
